ba1caec0e9d9063f60883643c7d62b9294761281
Diogo Brito <Diogo Brito>
2018-05-27 15:40:26 -0300

    Realizada pequenas estilizações em colunas do tipo 'list' do elemento ng2-smart-table

--- src/app/pages/challenges/create-challenge/create-challenge.component.ts
+++ src/app/pages/challenges/create-challenge/create-challenge.component.ts
@@ -7,11 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateChallengeComponent implements OnInit {
 
+  subm
+  data = [
+    {
+      title: 'Challenge 1',
+      discipline: 'Corporative Development',
+      submitted: 'False',
+      totalPontuation: 10,
+    },
+    {
+      title: 'Challenge 2',
+      discipline: 'Soft. Engeen',
+      submitted: 'True',
+      totalPontuation: 20,
+    }
+  ]
+
   settings = {
     add: {
       addButtonContent: '<i class="nb-plus"></i>',
       createButtonContent: '<i class="nb-checkmark"></i>',
       cancelButtonContent: '<i class="nb-close"></i>',
+      confirmCreate: true
     },
     edit: {
       editButtonContent: '<i class="nb-edit"></i>',
@@ -23,17 +40,36 @@ export class CreateChallengeComponent implements OnInit {
       confirmDelete: true,
     },
     columns: {
-      id: {
-        title: 'ID',
-        type: 'number',
-      },
       title: {
-        title: 'Title',
-        type: 'string',
+        title: 'Title'
+      },
+      discipline: {
+        title: 'Discipline',
+        editor: {
+          type: 'list'
+        },
+        filter: {
+          type: 'list',
+          config: {
+            selectText: 'Select...',
+            list: [
+              { value: 'Glenna Reichert', title: 'Glenna Reichert' },
+              { value: 'Kurtis Weissnat', title: 'Kurtis Weissnat' },
+              { value: 'Chelsey Dietrich', title: 'Chelsey Dietrich' },
+            ],
+          },
+        },
       },
       submitted: {
         title: 'Submitted',
-        type: 'string',
+        editor: {
+          type: 'list'
+        }
+      },
+      totalPontuation: {
+        title: 'Total pontuation',
+        addable: false,
+        editable: false
       }
     },
   };
@@ -41,6 +77,15 @@ export class CreateChallengeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onConfirmCreateEvent(event):void  {
+    if (window.confirm('Are you sure you want to create?')) {
+      event.newData['name'] += ' + added in code';
+      event.confirm.resolve(event.newData);
+    } else {
+      event.confirm.reject();
+    }
   }
 
   onDeleteConfirm(event): void {
